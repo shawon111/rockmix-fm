5e322e2b02dbc3c2b138ed46ddf774d67337785c
shawon111 <shawonahmmed08509@gmail.com>
2025-08-15 20:24:01 +0600

    build fix v2

--- src/app/new-releases/page.jsx
+++ src/app/new-releases/page.jsx
@@ -4,14 +4,14 @@ import TrackList from '@/components/Tracks/TrackList';
 import { baseURL } from '@/lib/baseURL';
 
 const NewReleases = async() => {
-    const apiUrl = `${baseURL}/api/tracks`;
-    const res = await fetch(apiUrl, {next: {revalidate: 3600}})
-    const tracks = await res.json();
+    // const apiUrl = `${baseURL}/api/tracks`;
+    // const res = await fetch(apiUrl, {next: {revalidate: 3600}})
+    // const tracks = await res.json();
     return (
         <div>
             <PageHero pageTitle="New Releases" />
             <div className="py-12">
-                <TrackList tracks={tracks.data} />
+                {/* <TrackList tracks={tracks.data} /> */}
             </div>
             <PageBottom />
         </div>
